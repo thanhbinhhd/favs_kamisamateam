135448f99045870cfb5a4471de86314e786cc818
Le Tat Tung <letattungtb@gmail.com>
2018-08-13 23:59:41 +0700

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   root "pages#show", page: "home"
   get "pages/:page", to: "pages#show", as: "page"
   resources :products, only: [:index, :show]
-  resources :users, only: :index
+  resources :users, only: [:index, :show]
 end
