0767db03cfa515e3548513b32bbfa6a54f5bce89
Le Tat Tung <letattungtb@gmail.com>
2018-08-19 22:19:40 +0700

    search feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   mount_uploader :image, ImageUploader
+
+  class << self
+    def search data
+      data = data.downcase
+      Product.where "lower(name) LIKE ?", "%#{data}%"
+    end
+  end
 end
